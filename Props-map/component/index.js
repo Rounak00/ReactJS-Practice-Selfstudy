d2bf82991c966dc74350efb46b16f0dfd5960570
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-11-07 18:15:22 +0530

    Update index.js

--- Props-map/component/index.js
+++ Props-map/component/index.js
@@ -3,13 +3,14 @@ import Data from './Data'
 import Card from './Card'
 ReactDom.render(
 <> 
-    {Data.map((item,index,Data)=>{
+    {Data.map((item,index)=>(
             <Card
+            key={index}
             name={item.name}
             salary={item.salary}
             state={item.state}
             />
-      })
+      ))
     }
 </>, 
 document.getElementById("root"));
