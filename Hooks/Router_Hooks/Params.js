4abea7a8c5ceb580bb7fe0c1fe4d64a7bf4a739c
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2023-01-22 23:19:19 +0530

    Update Params.js

--- Hooks/Router_Hooks/Params.js
+++ Hooks/Router_Hooks/Params.js
@@ -1,5 +1,24 @@
 import {useParams} from "react-router-dom";
  
+/* With out hook
+export function test({match}){
+  return (
+    <p>Hello {match.params.name}</p>
+  );
+}
+*/
+
  eport function test(){
-   const par=useParams();
+   const par=useParams(); //path should be /:name; // it return a object
+   return (
+    <p>Hello {par.name}</p>
+  );
+ }
+ 
+ 
+  eport function test(){
+   const par=useParams(); //path should be /:fname/:lname; // it return a object
+   return (
+    <p>Hello {par.fname} {par.lname}</p>
+  );
  }
