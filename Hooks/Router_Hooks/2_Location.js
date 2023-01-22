f8419b9c2970f7ebcc45006d9e21110df1fe42aa
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2023-01-22 23:37:26 +0530

    Update 2_Location.js

--- Hooks/Router_Hooks/2_Location.js
+++ Hooks/Router_Hooks/2_Location.js
@@ -1,5 +1,10 @@
-import {useLocation} from "react-router-dom"
+import {useLocation, useParams} from "react-router-dom"
 
-export function app(){
-
-};
+ export function test(){
+   const par=useParams();
+   const location=useLocation(); // return a object of hash,key, pathname, search etc;
+   return (
+    <p>Hello {par.fname} {par.lname}</p>
+     <p>Our path : {location.pathname}</p>
+  );
+ }
