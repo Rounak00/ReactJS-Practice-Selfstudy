4ad3f812fb38c45c552830cd54c86f5ef8f69691
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2023-02-11 02:50:34 +0530

    Update useDebounce.jsx

--- Hooks/8_useDebounce_CustomHook/useDebounce.jsx
+++ Hooks/8_useDebounce_CustomHook/useDebounce.jsx
@@ -1,1 +1,23 @@
+const [type,setType]=useState("");
 
+function handleInput(e){
+   const tar=e.target.value;
+   clearTimeout(type);
+  
+   const timeout=setTimeout(()=>{
+     Search(text);
+   },1000);
+  setType(timeout);
+}
+
+//Interm of component/Custo hook
+const [type,setType]=useState("");
+function handleInput(func,waiy=1000){
+  
+   clearTimeout(type);
+  
+   const timeout=setTimeout(()=>{
+     func()
+   },wait);
+  setType(timeout);
+}
