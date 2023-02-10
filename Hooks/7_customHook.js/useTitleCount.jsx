d90d8db22e87201b07b2e6fd198ceb00bc0d69d4
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2023-02-11 02:29:46 +0530

    Update and rename hooks to useTitleCount.jsx

---
+++ Hooks/7_customHook.js/useTitleCount.jsx
@@ -0,0 +1,10 @@
+
+import {useEffect, useState} from 'react';
+
+const useTitleCount=(count)=>{
+   useEffect(()=>{
+     if(count>=1)document.title=`chats (${count})`;
+     else document.title=`chat`;
+   },[count])
+}
+export default useTitleCount;
